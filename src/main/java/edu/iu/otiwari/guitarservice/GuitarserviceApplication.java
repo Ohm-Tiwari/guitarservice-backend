a472e1e8b4a73c8842144cdf55d85e9008fbb11e
Ohm-Tiwari <otiwari24@gmail.com>
2024-04-24 16:22:54 -0400

    commit

---
+++ src/main/java/edu/iu/otiwari/guitarservice/GuitarserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.otiwari.guitarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/otiwari/guitarservice/GuitarserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.otiwari.guitarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
